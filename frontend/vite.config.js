249e325d14e9241595e71a214279dea339a5ae13
Nagari Pavan <pawanmbnr03@gmail.com>
2025-09-23 18:39:08 +0530

    Configured Vite for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  base: "https://github.com/nagaripavan/Doctor-Appointment-Booking-Website.git",
 });
