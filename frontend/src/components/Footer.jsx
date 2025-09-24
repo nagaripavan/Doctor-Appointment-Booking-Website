fa00d7031f083e4a3f49f02e3b33918ddfad8f8b
Nagari Pavan <pawanmbnr03@gmail.com>
2025-09-24 23:36:38 +0530

    Designed Login and Sign up pages

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,15 +4,15 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='mt-40 mb-5'>
-        <div className='grid grid-cols-3 gap-8 mb-8 '>
+        <div className='flex flex-col gap-10 md:flex-row md:gap-35 mb-8 '>
             {/*------------Left section--------- */}
             <div className='w-1/2 '>
-                <img className='mb-5' src={assets.logo1} alt="" />
-                <p className='text-sm w-100'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <img className='w-1/3  mb-5' src={assets.logo1} alt="" />
+                <p className='text-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div>
 
             {/*------------center section--------- */}
-            <div className=' pl-50 pt-5'>
+            <div className=' pt-5'>
                 <h1 className='font-semibold text-l mb-2'>COMPANY</h1>
                 <ul className='text-gray-600'>
                     <li>Home</li>
@@ -23,7 +23,7 @@ const Footer = () => {
             </div>
 
             {/*------------Right section--------- */}
-            <div className='pl-20 pt-5'>
+            <div className=' pt-5'>
                 <h1 className='font-semibold text-l mb-2'>GET IN TOUCH</h1>
                 <p className='text-gray-600'>+919999999999</p>
                 <p className='text-gray-600'>nagari.pavan.03@gmail.com</p>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+  const [state,setState]=useState('sign up')
+
+  const [name,setName]=useState('')
+  const [Password,setPassword]=useState('')
+  const [email,setEmail]=useState('')
+
   return (
-    <div>Login</div>
+    <form className='flex justify-center'>
+      <div className='border border-gray-200 shadow-lg w-max my-20 rounded-2xl p-8 mx-10'>
+        <p className='text-2xl font-semibold text-gray-700 mb-4'>{state === 'sign up'?'Create Account':'Login'}</p>
+        <p className='text-sm mb-4 text-gray-600'>Please {state === 'sign up'?'sign up':'Login'} to book appointment</p>
+        {state === 'sign up'?
+            <div>
+          <p className='text-sm text-gray-600 font-semibold'>Full Name</p>
+          <input className='border border-gray-300 mt-2 mb-5 px-3 py-1 rounded-l w-80' type="text" onChange={(e)=>setName(e.target.name)} value={name} required/>
+        </div>:<div></div>      
+        }
+        <div>
+          <p className='text-sm text-gray-600 font-semibold'>Email</p>
+          <input className='border border-gray-300 mt-2 mb-5 px-3 py-1 w-80 rounded-l' type="text" onChange={(e)=>setEmail(e.target.name)} value={email} required/>
+        </div>
+        <div>
+          <p className='text-sm text-gray-600  font-semibold'>Password</p>
+          <input className='border border-gray-300 mt-2 mb-5 px-3 py-1 w-80 rounded-l' type="text" onChange={(e)=>setPassword(e.target.name)} value={Password} required/>
+        </div>
+        <button className={`bg-[#5f6FFF] text-white ${state === 'sign up'?'px-26':'px-35'} rounded-l py-2 cursor-pointer font-semibold mb-3`}>{state === 'sign up'?'Create Account':'Login'}</button>
+
+        <div className='text-sm'>
+          {state === 'sign up'?
+        <p>Already have an account? <span onClick={()=>setState('login')} className='text-blue-600 underline cursor-pointer'  >Login here</span></p>:
+        <p>Create an new account? <span onClick={()=>setState('sign up')} className='text-blue-600 underline cursor-pointer'>Click here</span></p>}
+        </div>
+      </div>
+    </form>
   )
 }
 
