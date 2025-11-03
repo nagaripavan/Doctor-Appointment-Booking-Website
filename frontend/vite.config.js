d89aa8e2d7d770c272a2e70bc19b8fa9fa28b735
Nagari Pavan <pawanmbnr03@gmail.com>
2025-11-03 11:22:55 +0530

    deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/Doctor-Appointment-Booking-Website/",
-  plugins: [tailwindcss(),react()]
+  plugins: [tailwindcss(),react()],
+  base : process.env.VITE_BASE_PATH || "/",
 });
