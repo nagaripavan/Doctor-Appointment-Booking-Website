11f8062143544925b43605a56718da4e95a63fe9
Nagari Pavan <pawanmbnr03@gmail.com>
2025-11-22 18:09:25 +0530

    designed admincontroller to save doctor details in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 
 //app config
@@ -14,7 +15,11 @@ connectCloudinary()
 //middlewares
 app.use(express.json())
 app.use(cors())
+
 //api end point 
+app.use('/api/admin',adminRouter)
+//localhost:4000/api/admin/add-doctor
+
 
 app.get('/',(req,res)=>{
     res.send('api working')
